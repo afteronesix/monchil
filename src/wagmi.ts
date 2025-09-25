5641a28967573c1526ec808f39953083e101d8dc
Shot <74735269+afteronesix@users.noreply.github.com>
2025-09-25 22:34:28 +0700

    Update wagmi.ts

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,12 +1,16 @@
 import { farcasterMiniApp as miniAppConnector } from '@farcaster/miniapp-wagmi-connector'
 import { http, createConfig } from 'wagmi'
+import { walletConnect } from 'wagmi/connectors'
 import { monadTestnet } from 'wagmi/chains'
 
-
+const projectId = 'b5177ed9c756b72ea8a9cb11f7aab606'
 
 export const config = createConfig({
   chains: [monadTestnet],
-  connectors: [miniAppConnector()],
+  connectors: [
+    miniAppConnector(),
+    walletConnect({ projectId }),
+  ],
   transports: {
     [monadTestnet.id]: http(),
   },
